6c91f7549dd6d5506f9273dfb5f1612f52d20cc0
Yiwen Wang <wangyiwenau@gmail.com>
2025-08-04 15:09:03 +1000

    fetch flowers in flowers page

--- src/app/flowers/page.tsx
+++ src/app/flowers/page.tsx
@@ -1,7 +1,18 @@
+import useFlowers from "@/hooks/useGetFlowers";
+
 const Flowers = () => {
+  const { data, isLoading, error } = useFlowers();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading flowers</div>;
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold">Our Flowers</h1>
+      <ul>
+        {data?.map((flower) => (
+          <li key={flower.id}>{flower.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
