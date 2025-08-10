08777139182dcfb242f09aa2cd4316a19aad1611
Yiwen Wang <wangyiwenau@gmail.com>
2025-08-10 15:34:32 +1000

    add react form to addnewflower component

--- src/components/ui/AddNewFlower.tsx
+++ src/components/ui/AddNewFlower.tsx
@@ -1,10 +1,34 @@
+"use client";
 import { Plus } from "lucide-react";
 import { Dialog } from "radix-ui";
 import { Cross2Icon } from "@radix-ui/react-icons";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
+
+type Inputs = {
+  name: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  productType: string;
+  occasion: string;
+  color: string;
+  discount?: number;
+};
 
 const AddNewFlower = () => {
+  const [open, setOpen] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    setOpen(false);
+  };
   return (
-    <Dialog.Root>
+    <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger asChild>
         <div className="group border rounded-md overflow-hidden hover:cursor-pointer lg:w-[23%] md:w-[30%] sm:w-[45%]">
           <div className="relative bg-stone-200 aspect-square">
@@ -28,41 +52,135 @@ const AddNewFlower = () => {
             Create new flower
           </Dialog.Title>
           <Dialog.Description className="mb-5 mt-2.5 text-[15px] leading-normal text-mauve11">
-            Make changes to your profile here. Click save when you are done.
+            Create your new flower here. Click save when you are done.
           </Dialog.Description>
-          <fieldset className="mb-[15px] flex items-center gap-5">
-            <label
-              className="w-[90px] text-right text-[15px] text-violet11"
-              htmlFor="name"
-            >
-              Name
-            </label>
-            <input
-              className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
-              id="name"
-              defaultValue="Pedro Duarte"
-            />
-          </fieldset>
-          <fieldset className="mb-[15px] flex items-center gap-5">
-            <label
-              className="w-[90px] text-right text-[15px] text-violet11"
-              htmlFor="username"
-            >
-              Username
-            </label>
-            <input
-              className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
-              id="username"
-              defaultValue="@peduarte"
-            />
-          </fieldset>
-          <div className="mt-[25px] flex justify-end">
-            <Dialog.Close asChild>
-              <button className="inline-flex h-[35px] items-center justify-center rounded bg-green4 px-[15px] font-medium leading-none text-green11 outline-none outline-offset-1 hover:bg-green5 focus-visible:outline-2 focus-visible:outline-green6 select-none">
-                Save changes
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <fieldset className="mb-[15px] flex items-center gap-5">
+              <label
+                className="w-[90px] text-right text-[15px] text-violet11"
+                htmlFor="name"
+              >
+                Name
+              </label>
+              <input
+                className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
+                id="name"
+                {...register("name", { required: true })}
+              />
+            </fieldset>
+            <fieldset className="mb-[15px] flex items-center gap-5">
+              <label
+                className="w-[90px] text-right text-[15px] text-violet11"
+                htmlFor="description"
+              >
+                Description
+              </label>
+              <textarea
+                className="inline-flex pt-2 h-[100px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
+                id="description"
+                {...register("description", { required: true })}
+              />
+            </fieldset>
+            <fieldset className="mb-[15px] flex items-center gap-5">
+              <label
+                className="w-[90px] text-right text-[15px] text-violet11"
+                htmlFor="image"
+              >
+                Image
+              </label>
+              <input
+                className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
+                id="image"
+                {...register("imageUrl", { required: true })}
+              />
+            </fieldset>
+            <fieldset className="mb-[15px] flex items-center gap-5">
+              <label
+                className="w-[90px] text-right text-[15px] text-violet11"
+                htmlFor="productType"
+              >
+                Product Type
+              </label>
+              <select
+                id="productType"
+                className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
+                {...register("productType", { required: true })}
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  Select a type
+                </option>
+                <option value="Flower">Flower</option>
+                <option value="Bouquet">Bouquet</option>
+                <option value="Plant">Plant</option>
+              </select>
+            </fieldset>
+            <fieldset className="mb-[15px] flex items-center gap-5">
+              <label
+                className="w-[90px] text-right text-[15px] text-violet11"
+                htmlFor="color"
+              >
+                Color
+              </label>
+              <select
+                id="color"
+                className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
+                {...register("color", { required: true })}
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  Select a color
+                </option>
+                <option value="Red">Red</option>
+                <option value="Blue">Blue</option>
+                <option value="Yellow">Yellow</option>
+              </select>
+            </fieldset>
+            <fieldset className="mb-[15px] flex items-center gap-5">
+              <label
+                className="w-[90px] text-right text-[15px] text-violet11"
+                htmlFor="occasion"
+              >
+                Occasion
+              </label>
+              <select
+                id="occasion"
+                className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
+                {...register("occasion", { required: true })}
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  Select an occasion
+                </option>
+                <option value="Birthday">Birthday</option>
+                <option value="Anniversary">Anniversary</option>
+                <option value="Sympathy">Sympathy</option>
+              </select>
+            </fieldset>
+            <fieldset className="mb-[15px] flex items-center gap-5">
+              <label
+                className="w-[90px] text-right text-[15px] text-violet11"
+                htmlFor="discount"
+              >
+                Discount
+              </label>
+              <input
+                className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
+                id="discount"
+                defaultValue={0}
+                {...register("discount")}
+              />
+            </fieldset>
+
+            <div className="mt-[25px] flex justify-end">
+              <button
+                type="submit"
+                className="inline-flex h-[35px] items-center justify-center rounded bg-green4 px-[15px] font-medium leading-none text-green11 outline-none outline-offset-1 hover:bg-green5 focus-visible:outline-2 focus-visible:outline-green6 select-none"
+              >
+                Create Flower
               </button>
-            </Dialog.Close>
-          </div>
+            </div>
+          </form>
           <Dialog.Close asChild>
             <button
               className="absolute right-2.5 top-2.5 inline-flex size-[25px] appearance-none items-center justify-center rounded-full text-violet11 bg-gray3 hover:bg-violet4 focus:shadow-[0_0_0_2px] focus:shadow-violet7 focus:outline-none"
