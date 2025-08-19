b9e73315dc2a4f352cf64a6f2b2dd77ec6c55cd9
Yiwen Wang <wangyiwenau@gmail.com>
2025-08-19 14:27:40 +1000

    add new font family

--- src/app/flowers/[id]/page.tsx
+++ src/app/flowers/[id]/page.tsx
@@ -2,17 +2,32 @@
 
 import { use } from "react";
 import useGetFlowerById from "@/hooks/useGetFlowerById";
+import Image from "next/image";
+import { Jost } from "next/font/google";
+
+const jost = Jost({
+  weight: "500",
+  subsets: ["latin"],
+  style: "italic",
+});
+
 const FlowerPage = ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = use(params);
   const { data, isPending, isError } = useGetFlowerById(Number(id));
+
   console.log(data);
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="text-2xl sm:text-4xl pt-20 pb-2 px-1 sm:px-8 font-sans font-medium">
-        Our Best Selling Flowers
+    <div className="flex flex-row gap-50 min-h-screen pt-30 pb-2 px-1 sm:px-8">
+      <div className="w-4/5">
+        <Image src={data?.imageUrl} alt={data?.name} width={900} height={900} />
       </div>
-      <div>Product ID: {id}</div>
-      <div>{data?.name}</div>
+      <div className="flex flex-col gap-5 w-4/5">
+        <div className="text-5xl font-medium tracking-wide">{data?.name}</div>
+        <div className={`text-3xl font-bold ${jost.className}`}>
+          AUS ${data?.price}
+        </div>
+        <div className={`text-text-gray text-lg `}>{data?.description}</div>
+      </div>
     </div>
   );
 };
