17ceae78138dcc871ae46ae0501e5cf6f64c6bda
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-01 17:05:47 +1000

    loading google map script in the page instead of form

--- src/app/checkout/CheckoutPageClient.tsx
+++ src/app/checkout/CheckoutPageClient.tsx
@@ -5,6 +5,7 @@ import RecipientForm from "@/components/features/checkout/RecipientForm";
 import SenderForm from "@/components/features/checkout/SenderForm";
 import SenderInfo from "@/components/features/checkout/SenderInfo";
 import { useAppSelector } from "@/redux/hooks";
+import { LoadScript } from "@react-google-maps/api";
 import { redirect } from "next/navigation";
 
 const CheckoutPageClient = () => {
@@ -14,33 +15,39 @@ const CheckoutPageClient = () => {
   });
   const sender = useAppSelector((s) => s.sender);
   if (items.length == 0) redirect("/");
+  const libraries: "places"[] = ["places"];
   return (
-    <div className="px-40 py-20 grid grid-cols-[2fr_1fr] gap-40">
-      <div>
-        <h1 className="text-4xl py-10">Checkout</h1>
-        {sender.mode == "form" ? (
-          <SenderForm />
-        ) : sender.mode == "info" ? (
-          <>
-            <SenderInfo />
-            <RecipientForm />
-          </>
-        ) : null}
+    <LoadScript
+      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
+      libraries={libraries}
+    >
+      <div className="px-40 py-20 grid grid-cols-[2fr_1fr] gap-40">
+        <div>
+          <h1 className="text-4xl py-10">Checkout</h1>
+          {sender.mode == "form" ? (
+            <SenderForm />
+          ) : sender.mode == "info" ? (
+            <>
+              <SenderInfo />
+              <RecipientForm />
+            </>
+          ) : null}
+        </div>
+        <div className="py-10">
+          {items.map((item) => (
+            <CartCardCheckout
+              key={item.id}
+              name={item.name}
+              size={item.size}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              giftQty={item.giftQty}
+            />
+          ))}
+          <CartSubtotalCheckout />
+        </div>
       </div>
-      <div className="py-10">
-        {items.map((item) => (
-          <CartCardCheckout
-            key={item.id}
-            name={item.name}
-            size={item.size}
-            price={item.price}
-            imageUrl={item.imageUrl}
-            giftQty={item.giftQty}
-          />
-        ))}
-        <CartSubtotalCheckout />
-      </div>
-    </div>
+    </LoadScript>
   );
 };
 export default CheckoutPageClient;
--- src/components/features/checkout/RecipientForm.tsx
+++ src/components/features/checkout/RecipientForm.tsx
@@ -4,7 +4,6 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import PlacesAutocomplete from "./PlacesAutocomplete ";
 import { RecipientFormValues } from "@/libs/types/forms";
 
-const libraries: "places"[] = ["places"];
 const RecipientForm = () => {
   const {
     register,
@@ -18,144 +17,139 @@ const RecipientForm = () => {
     console.log(data);
   };
   return (
-    <LoadScript
-      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
-      libraries={libraries}
-    >
-      <div className="w-4/5 pt-10 border-t-2 border-gray-200 mt-10">
-        <span className="flex flex-row gap-5 items-center">
-          <span className="text-xl flex items-center justify-center w-12 h-12 text-white bg-black rounded-full">
-            2
-          </span>
-          <span className="text-2xl">Recipient Details</span>
+    <div className="w-4/5 pt-10 border-t-2 border-gray-200 mt-10">
+      <span className="flex flex-row gap-5 items-center">
+        <span className="text-xl flex items-center justify-center w-12 h-12 text-white bg-black rounded-full">
+          2
         </span>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col py-10 gap-3">
-            <label className="text-xl">First Name</label>
-            <input
-              type="text"
-              {...register("firstName", { required: "This field is required" })}
-              placeholder="Enter your first name"
-              className="w-full p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
-            />
-            {errors.firstName && (
-              <span className="mt-2 text-sm text-red-600">
-                {errors.firstName.message}
-              </span>
-            )}
-            <label className="text-xl">Last Name</label>
-            <input
-              type="text"
-              {...register("lastName", { required: "This field is required" })}
-              placeholder="Enter your last name"
-              className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
-            />
-            {errors.lastName && (
-              <span className="mt-2 text-sm text-red-600">
-                {errors.lastName.message}
-              </span>
-            )}
-            <label className="text-xl">Phone Number</label>
-            <input
-              type="text"
-              {...register("phone", {
-                required: "This field is required",
-                pattern: {
-                  value: /^\d{10}$/,
-                  message: "Phone must be exactly 10 digits",
-                },
-              })}
-              placeholder="Enter your phone number"
-              className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
-            />
-            {errors.phone && (
-              <span className="mt-2 text-sm text-red-600">
-                {errors.phone.message}
-              </span>
-            )}
-            <label className="text-xl">Address</label>
-            <PlacesAutocomplete control={control} />
-            <div className="flex flex-row justify-between">
-              <div className="flex flex-col gap-2 items-end ">
-                <div className="flex flex-row gap-4 items-center">
-                  <label className="text-xl">Suburb</label>
-                  <input
-                    type="text"
-                    {...register("suburb", {
-                      required: "This field is required",
-                    })}
-                    placeholder="Enter your suburb"
-                    className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
-                  />
-                </div>
-                {errors.suburb && (
-                  <span className="mt-2 text-sm text-red-600 place-content-end">
-                    {errors.suburb && (
-                      <span className="mt-2 text-sm text-red-600">
-                        {errors.suburb.message}
-                      </span>
-                    )}
-                  </span>
-                )}
+        <span className="text-2xl">Recipient Details</span>
+      </span>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col py-10 gap-5">
+          <label className="text-xl">First Name</label>
+          <input
+            type="text"
+            {...register("firstName", { required: "This field is required" })}
+            placeholder="Enter your first name"
+            className="w-full p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
+          />
+          {errors.firstName && (
+            <span className="mt-2 text-sm text-red-600">
+              {errors.firstName.message}
+            </span>
+          )}
+          <label className="text-xl">Last Name</label>
+          <input
+            type="text"
+            {...register("lastName", { required: "This field is required" })}
+            placeholder="Enter your last name"
+            className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
+          />
+          {errors.lastName && (
+            <span className="mt-2 text-sm text-red-600">
+              {errors.lastName.message}
+            </span>
+          )}
+          <label className="text-xl">Phone Number</label>
+          <input
+            type="text"
+            {...register("phone", {
+              required: "This field is required",
+              pattern: {
+                value: /^\d{10}$/,
+                message: "Phone must be exactly 10 digits",
+              },
+            })}
+            placeholder="Enter your phone number"
+            className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
+          />
+          {errors.phone && (
+            <span className="mt-2 text-sm text-red-600">
+              {errors.phone.message}
+            </span>
+          )}
+          <label className="text-xl">Address</label>
+          <PlacesAutocomplete control={control} />
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col gap-2 items-end ">
+              <div className="flex flex-row gap-4 items-center">
+                <label className="text-xl">Suburb</label>
+                <input
+                  type="text"
+                  {...register("suburb", {
+                    required: "This field is required",
+                  })}
+                  placeholder="Enter your suburb"
+                  className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
+                />
               </div>
-              <div className="flex flex-col gap-2 items-end">
-                <div className="flex flex-row gap-4 items-center">
-                  <label className="text-xl">Postcode</label>
-                  <input
-                    type="text"
-                    {...register("postcode", {
-                      required: "This field is required",
-                    })}
-                    placeholder="Enter your postcode"
-                    className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
-                  />
-                </div>
-                {errors.postcode && (
-                  <span className="mt-2 text-sm text-red-600 place-content-end">
-                    {errors.postcode.message}
-                  </span>
-                )}
-              </div>
+              {errors.suburb && (
+                <span className="mt-2 text-sm text-red-600 place-content-end">
+                  {errors.suburb && (
+                    <span className="mt-2 text-sm text-red-600">
+                      {errors.suburb.message}
+                    </span>
+                  )}
+                </span>
+              )}
             </div>
-
-            <label className="text-xl">Phone Number</label>
-            <input
-              type="text"
-              {...register("phone", {
-                required: "This field is required",
-                pattern: {
-                  value: /^\d{10}$/,
-                  message: "Phone must be exactly 10 digits",
-                },
-              })}
-              placeholder="Enter your phone number"
-              className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
-            />
-            {errors.phone && (
-              <span className="mt-2 text-sm text-red-600">
-                {errors.phone.message}
-              </span>
-            )}
-            <label className="text-xl">Card Message</label>
-            <textarea
-              {...register("message")}
-              placeholder="Enter your message"
-              className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
-            />
-            <label className="text-xl">Delivery Instructions</label>
-            <textarea
-              {...register("deliveryInstructions")}
-              placeholder="Enter your delivery instructions"
-              className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
-            />
+            <div className="flex flex-col gap-2 items-end">
+              <div className="flex flex-row gap-4 items-center">
+                <label className="text-xl">Postcode</label>
+                <input
+                  type="text"
+                  {...register("postcode", {
+                    required: "This field is required",
+                  })}
+                  placeholder="Enter your postcode"
+                  className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
+                />
+              </div>
+              {errors.postcode && (
+                <span className="mt-2 text-sm text-red-600 place-content-end">
+                  {errors.postcode.message}
+                </span>
+              )}
+            </div>
           </div>
 
-          <Button variant="next" onClick={handleSubmit(onSubmit)}>
-            Next
-          </Button>
-        </form>
-      </div>
-    </LoadScript>
+          <label className="text-xl">Phone Number</label>
+          <input
+            type="text"
+            {...register("phone", {
+              required: "This field is required",
+              pattern: {
+                value: /^\d{10}$/,
+                message: "Phone must be exactly 10 digits",
+              },
+            })}
+            placeholder="Enter your phone number"
+            className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
+          />
+          {errors.phone && (
+            <span className="mt-2 text-sm text-red-600">
+              {errors.phone.message}
+            </span>
+          )}
+          <label className="text-xl">Card Message</label>
+          <textarea
+            {...register("message")}
+            placeholder="Enter your message"
+            className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
+          />
+          <label className="text-xl">Delivery Instructions</label>
+          <textarea
+            {...register("deliveryInstructions")}
+            placeholder="Enter your delivery instructions"
+            className="p-5 border-2 border-gray-200 rounded focus:border-black focus:ring-0 focus:outline-none"
+          />
+        </div>
+
+        <Button variant="next" onClick={handleSubmit(onSubmit)}>
+          Next
+        </Button>
+      </form>
+    </div>
   );
 };
 export default RecipientForm;
