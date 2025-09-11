a73a92ff8758e60faf82235357efb9925712c1ab
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-11 12:21:24 +1000

    add different banner to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,87 @@
-import ExploreFlowersBanner from "@/components/features/banner/components/ExploreFlowersBanner";
+import Banner from "@/components/features/banner/components/Banner";
 import Card from "@/components/ui/Card";
 import CardWithWords from "@/components/ui/CardWithWords";
 import CardWithWordsSec from "@/components/ui/CardWithWordsSec";
 import ShopLocation from "@/components/ui/ShopLocation";
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+});
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen w-full">
       <main>
-        <ExploreFlowersBanner />
-        <div className="text-2xl sm:text-4xl pt-20 pb-2 px-1 sm:px-8 font-sans font-medium">
-          Our Best Selling Flowers
-        </div>
+        <Banner
+          imageUrl="/1014_PF_Fall25-Site_Fall_Hero.webp"
+          headline="Our most-loved bouquets"
+          description="Best sellers for a reason—send something beautiful today."
+          name="Shop Now"
+          link="/flowers"
+          position="right"
+        />
         <div
-          className="relative inline-block after:absolute after:left-1/2 after:-bottom-1 after:h-[2px]
-           after:w-full after:-translate-x-1/2 after:bg-black after:content-[''] after:origin-center
-            after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100"
+          className={`text-2xl sm:text-4xl pt-20 pb-2 px-1 sm:px-8 font-sans font-medium ${raleway.className}`}
         >
-          Hover me
+          Our Best Selling Flowers
         </div>
         <div className="flex flex-row flex-wrap gap-8 py-5 px-1 sm:px-8 items-center">
           <Card id={1} title="Red Flowers" price="$19.99" image="/1.jpg" />
           <Card id={2} title="Blue Flowers" price="$29.99" image="/2.jpg" />
           <Card id={3} title="Yellow Flowers" price="$39.99" image="/3.jpg" />
           <Card id={4} title="White Flowers" price="$49.99" image="/4.jpg" />
+          <Card id={1} title="Red Flowers" price="$19.99" image="/1.jpg" />
+          <Card id={2} title="Blue Flowers" price="$29.99" image="/2.jpg" />
+          <Card id={3} title="Yellow Flowers" price="$39.99" image="/3.jpg" />
+          <Card id={4} title="White Flowers" price="$49.99" image="/4.jpg" />
         </div>
+        <Banner
+          imageUrl="/1014_PF_Fall25-Site_Anniversary_Hero.webp"
+          headline="Shop by Occasions"
+          description="Find the perfect bouquet for every special moment—birthdays, anniversaries, celebrations, and more."
+          name="Browse Occasions"
+          link="/occasions"
+          position="left"
+        />
+        <div
+          className={`text-2xl sm:text-4xl pt-20 pb-2 px-1 sm:px-8 font-sans font-medium ${raleway.className}`}
+        >
+          Choose your Flowers by Occasions
+        </div>
+        <div className="flex flex-row flex-wrap gap-8 py-5 px-1 sm:px-8 items-center">
+          <Card id={1} title="Red Flowers" price="$19.99" image="/1.jpg" />
+          <Card id={2} title="Blue Flowers" price="$29.99" image="/2.jpg" />
+          <Card id={3} title="Yellow Flowers" price="$39.99" image="/3.jpg" />
+          <Card id={4} title="White Flowers" price="$49.99" image="/4.jpg" />
+          <Card id={1} title="Red Flowers" price="$19.99" image="/1.jpg" />
+          <Card id={2} title="Blue Flowers" price="$29.99" image="/2.jpg" />
+          <Card id={3} title="Yellow Flowers" price="$39.99" image="/3.jpg" />
+          <Card id={4} title="White Flowers" price="$49.99" image="/4.jpg" />
+        </div>
+        <Banner
+          imageUrl="/0908_Bouqsday_HPUpdate_Desktop.jpg"
+          headline="Shop by Flower Types"
+          description="Choose from roses, lilies, tulips, and more—discover bouquets made from your favorite blooms."
+          name="Explore Flowers"
+          link="/flowers/types"
+          position="left"
+        />
+        <div
+          className={`text-2xl sm:text-4xl pt-20 pb-2 px-1 sm:px-8 font-sans font-medium ${raleway.className}`}
+        >
+          Choose Your Flowers by Types
+        </div>
+        <div className="flex flex-row flex-wrap gap-8 py-5 px-1 sm:px-8 items-center">
+          <Card id={1} title="Red Flowers" price="$19.99" image="/1.jpg" />
+          <Card id={2} title="Blue Flowers" price="$29.99" image="/2.jpg" />
+          <Card id={3} title="Yellow Flowers" price="$39.99" image="/3.jpg" />
+          <Card id={4} title="White Flowers" price="$49.99" image="/4.jpg" />
+          <Card id={1} title="Red Flowers" price="$19.99" image="/1.jpg" />
+          <Card id={2} title="Blue Flowers" price="$29.99" image="/2.jpg" />
+          <Card id={3} title="Yellow Flowers" price="$39.99" image="/3.jpg" />
+          <Card id={4} title="White Flowers" price="$49.99" image="/4.jpg" />
+        </div>
+        <ShopLocation />
         <div
           className="relative text-2xl text-violet1 sm:text-4xl pt-20 pb-2 px-1 sm:px-8 font-sans font-medium
              after:absolute after:left-1/2 after:-bottom-1 after:h-[2px] after:w-full
@@ -62,7 +120,6 @@ export default function Home() {
           <CardWithWordsSec title="Green" image="/3.jpg" color="green" />
           <CardWithWordsSec title="White" image="/4.jpg" color="gray" />
         </div>
-        <ShopLocation />
       </main>
     </div>
   );
--- src/components/features/banner/components/BannerCard.tsx
+++ src/components/features/banner/components/BannerCard.tsx
@@ -17,9 +17,11 @@ const BannerCard = ({
 }) => {
   return (
     <div
-      className={`absolute rounded inset-0 bg-secondary bg-opacity-50 w-fit h-fit  ${
-        position === "left" ? "left-1/3" : "right-1/3"
-      } flex flex-col justify-center items-left gap-4 py-10 px-14`}
+      className={`absolute rounded inset-0 bg-secondary bg-opacity-50 w-fit h-fit  
+        ${
+          position === "left" ? "left-[23%]" : "left-[72%]"
+        } top-1/2 -translate-y-1/2 -translate-x-1/2
+         flex flex-col justify-center items-left gap-4 py-10 px-14`}
     >
       <h2 className={`relative z-10 ${marcellus.className} text-4xl`}>
         {headline}
