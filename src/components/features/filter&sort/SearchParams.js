17240d7f005e7666260cfff22c509b0ba688944f
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-17 10:09:06 +1000

    add searchParams

---
+++ src/components/features/filter&sort/SearchParams.js
@@ -0,0 +1,62 @@
+export const filters = [
+  {
+    filterLabel: "occasion",
+    filterValue: [
+      "birthday",
+      "sympathy",
+      "thankyou",
+      "getwell",
+      "funeral",
+      "newbaby",
+      "congratulations",
+      "other",
+    ],
+  },
+  {
+    filterLabel: "color",
+    filterLabel: [
+      "red",
+      "white",
+      "yellow",
+      "pink",
+      "green",
+      "orange",
+      "purple",
+      "mixed",
+    ],
+  },
+  {
+    filterLabel: "productType",
+    filterValue: [
+      "box",
+      "bouquet",
+      "vase",
+      "basket",
+      "funeral",
+      "hamper",
+      "plant",
+      "succulent",
+      "driedflower",
+      "other",
+    ],
+  },
+  {
+    filterLabel: "flowerType",
+    filterValue: [
+      { label: "Roses", value: 1 },
+      { label: "Chrysanthemums", value: 2 },
+      { label: "Carnations", value: 3 },
+      { label: "Natives", value: 4 },
+      { label: "Gerberas", value: 5 },
+      { label: "Orchids", value: 6 },
+      { label: "Lilies", value: 7 },
+      { label: "Tropicals", value: 8 },
+      { label: "Sunflowers", value: 9 },
+      { label: "Irises", value: 10 },
+      { label: "Tulips", value: 11 },
+      { label: "Other", value: 12 },
+    ],
+  },
+];
+
+export const sorts = ["PriceAsc", "PriceDesc"];
--- src/components/features/filter&sort/components/FilterRow.tsx
+++ src/components/features/filter&sort/components/FilterRow.tsx
@@ -1,2 +1,8 @@
-const FilterRow = () => {};
+import { filters } from "../SearchParams";
+import Filter from "./Filter"
+const FilterRow = () => {
+  return (
+    <div className="flex flex-row gap-2">{filters.map((filter) => {<Filter name={filter.}/>})}</div>
+  );
+};
 export default FilterRow;
