1411b8d64095a154c03332cd94aadc2f860d2c4f
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-18 11:12:41 +1000

    create hook to remove filter

--- src/components/features/filter&sort/components/FilterIndicator.tsx
+++ src/components/features/filter&sort/components/FilterIndicator.tsx
@@ -1,8 +1,9 @@
+"use client";
 import { Cross2Icon } from "@radix-ui/react-icons";
 const FilterIndicator = ({ name, value }: { name: string; value: string }) => {
   return (
     <div className="flex flex-row items-center justify-between gap-3">
-      <div className=" text-sm capitalize">{value}</div>
+      <div className="text-sm capitalize">{value}</div>
       <Cross2Icon />
     </div>
   );
---
+++ src/components/features/filter&sort/hooks/useRemoveFilter.ts
@@ -0,0 +1,13 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+
+const useRemoveFilter = () => {
+  const searchParams = useSearchParams();
+
+  const removeFilter = (name: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete(name, value);
+  };
+  return { removeFilter };
+};
+export default useRemoveFilter;
