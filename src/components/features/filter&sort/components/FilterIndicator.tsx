9fcd94a52ccef5cf53805c8084529e36b879d5cd
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-19 11:12:13 +1000

    separate logic into hooks

--- src/components/features/filter&sort/components/FilterIndicator.tsx
+++ src/components/features/filter&sort/components/FilterIndicator.tsx
@@ -1,17 +1,12 @@
 "use client";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import useRemoveFilter from "../hooks/useRemoveFilter";
-import { filterMultiData } from "../SearchParams";
+import useGetFlowerTypeById from "../hooks/useGetFlowerTypeById";
 
 const FilterIndicator = ({ name, value }: { name: string; value: string }) => {
-  let filter;
-  const filterValue = filterMultiData.find(
-    (filter) => filter.filterLabel == "flowerType",
-  )?.filterValue;
-  if (name == "flowerType") {
-    filter = filterValue?.find((r) => r.value == value)?.label;
-  } else filter = value;
   const { removeFilter } = useRemoveFilter();
+  const { getFlowerTypeById } = useGetFlowerTypeById();
+  const filter = getFlowerTypeById(name, value);
   return (
     <div
       className="flex flex-row items-center justify-between gap-3"
---
+++ src/components/features/filter&sort/hooks/useGetFlowerTypeById.ts
@@ -0,0 +1,16 @@
+import { filterMultiData } from "../SearchParams";
+
+const useGetFlowerTypeById = () => {
+  const filterValue = filterMultiData.find(
+    (filter) => filter.filterLabel == "flowerType",
+  )?.filterValue;
+  const getFlowerTypeById = (name: string, value: string) => {
+    let filter;
+    if (name == "flowerType") {
+      filter = filterValue?.find((r) => r.value == value)?.label;
+    } else filter = value;
+    return filter;
+  };
+  return { getFlowerTypeById };
+};
+export default useGetFlowerTypeById;
