f27369cc0dc101de440d09e9ea974755ea633035
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-20 09:32:05 +1000

    shopping cart icon get item number indicator now

--- src/components/features/cart/components/CartButton.tsx
+++ src/components/features/cart/components/CartButton.tsx
@@ -1,14 +1,23 @@
 "use client";
 import { useCartContext } from "@/contexts/CartContext";
 import { ShoppingCart } from "lucide-react";
+import useGetCartItemNumber from "../hooks/useGetCartItemNumber";
 
 const CartButton = () => {
   const { handleCartOpen } = useCartContext();
+  const itemNumber = useGetCartItemNumber();
   return (
-    <ShoppingCart
-      className="h-6 w-6 cursor-pointer hover:text-green-800"
-      onClick={handleCartOpen}
-    />
+    <div className="relative">
+      <ShoppingCart
+        className="h-6 w-6 cursor-pointer text-gray-700 transition-colors hover:text-green-700"
+        onClick={handleCartOpen}
+      />
+      {itemNumber > 0 && (
+        <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-green-700 text-xs font-medium text-white shadow-md">
+          {itemNumber}
+        </span>
+      )}
+    </div>
   );
 };
 export default CartButton;
