8d1efdd6ad6a4a71aca86fcfdce235d71da34558
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-22 09:45:36 +1000

    create type for plants

---
+++ src/libs/types/plants.ts
@@ -0,0 +1,48 @@
+export type NewPlant = {
+  name: string;
+  description: string;
+  imageFile?: FileList;
+  price: number;
+  plantType: string;
+  discount: number;
+};
+export type CreatePlantDto = {
+  name: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  plantType: string;
+  discount: number;
+};
+export type CreatePlantResponse = {
+  name: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  plantType: string;
+  discount: number;
+};
+
+export type UploadImageResponse = {
+  url: string;
+};
+
+export type GetPlantsResponse = {
+  id: number;
+  name: string;
+  price: number;
+  discount?: number;
+  description?: string;
+  available: boolean;
+  imageUrl: string;
+}[];
+
+export type GetFlowerResponse = {
+  id: number;
+  name: string;
+  price: number;
+  discount?: number;
+  description?: string;
+  available: boolean;
+  imageUrl: string;
+};
