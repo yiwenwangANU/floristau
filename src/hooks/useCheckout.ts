907d740a32dc545c7aab2a8a3f7dc68536cc5119
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-30 09:51:47 +1000

    create useCheckout

---
+++ src/hooks/useCheckout.ts
@@ -0,0 +1,23 @@
+import { checkoutApi } from "@/libs/api/checkoutApi";
+import { StripePayRequest } from "@/libs/types/checkout";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
+
+const useCheckout = () => {
+  const router = useRouter();
+  return useMutation({
+    mutationFn: async (request: StripePayRequest) => {
+      const response = await checkoutApi.checkout(request);
+      return response;
+    },
+    onSuccess: (url) => {
+      router.push(url);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
+
+export default useCheckout;
