cd3fb8e607f71b14cfa14427b05bffba77fc4edd
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-30 10:51:44 +1000

    fix hydration error for cartButton

--- src/components/features/cart/components/CartButton.tsx
+++ src/components/features/cart/components/CartButton.tsx
@@ -2,17 +2,21 @@
 import { useCartContext } from "@/contexts/CartContext";
 import { ShoppingCart } from "lucide-react";
 import useGetCartItemNumber from "../hooks/useGetCartItemNumber";
+import { useEffect, useState } from "react";
 
 const CartButton = () => {
   const { handleCartOpen } = useCartContext();
   const itemNumber = useGetCartItemNumber();
+  // render badge after mount
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
   return (
     <div className="relative">
       <ShoppingCart
         className="h-6 w-6 cursor-pointer text-gray-700 transition-colors hover:text-green-700"
         onClick={handleCartOpen}
       />
-      {itemNumber > 0 && (
+      {mounted && itemNumber > 0 && (
         <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-green-700 text-xs font-medium text-white shadow-md">
           {itemNumber}
         </span>
